d0a2dff003fe734e2ff7fa80defece4ca4afc5cd
Ali M. Ismail <aismail@solostudio.co>
2016-03-19 14:50:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,9 @@
 			
 			
 
-			camera.position.x = 26;
-			camera.position.y = 15;
-			camera.position.z = 21;
+			camera.position.x = 0;
+			camera.position.y = 0;
+			camera.position.z = -21;
 			camera.lookAt(scene.position);
 			//controls = new THREE.OrbitControls( camera, renderer.domElement );
 
